c8c9984c6e00ca471db5e8408832c67b97cf46af
rizmiya-ameen <rizmiyaamin@gmail.com>
2024-01-17 22:31:35 +1100

    HomeBanner added

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import { products } from "@/utils/products";
 import { truncateText } from "@/utils/truncateText";
 import Container from "@/components/Container";
 import ProductCard from "@/components/products/ProductCard";
+import HomeBanner from "@/components/HomeBanner";
 
 export default function Home() {
   return (
     <div className="p-8">
       <Container>
-        <div>{/* <HomeBanner />*/}</div>
+        <div><HomeBanner /></div>
 
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product) => {
