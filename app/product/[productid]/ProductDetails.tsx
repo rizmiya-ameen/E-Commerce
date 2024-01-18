8ef77c124ff4e1f32dfaa0904ac5c9dabd1507db
rizmiya-ameen <rizmiyaamin@gmail.com>
2024-01-18 18:19:15 +1100

    set color

--- app/product/[productid]/ProductDetails.tsx
+++ app/product/[productid]/ProductDetails.tsx
@@ -9,50 +9,61 @@ interface ProductDetailsProps {
 }
 
 export type CartProductType = {
-  id: string,
-  name: string,
-  description: string,
-  category: string,
-  brand: string,
-  selectedImg: SelectedImgType,
-  quantity: number,
-  price: number
-}
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  brand: string;
+  selectedImg: SelectedImgType;
+  quantity: number;
+  price: number;
+};
 
 export type SelectedImgType = {
-  color: string,
-  colorCode: string,
-  image: string,
-}
+  color: string;
+  colorCode: string;
+  image: string;
+};
 
 const Horizontal = () => {
-  return <hr className="w-[30%] my-2"/>
-}
+  return <hr className="w-[30%] my-2" />;
+};
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
-
-  const [ cartProduct, setCartProduct ] = useState<CartProductType>({
+  
+  const [cartProduct, setCartProduct] = useState<CartProductType>({
     id: product.id,
     name: product.name,
     description: product.description,
     category: product.category,
     brand: product.brand,
-    selectedImg: {...product.images[0]},
+    selectedImg: { ...product.images[0] },
     quantity: 1,
     price: product.price,
-  })
+  });
 
-  const productRating = product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) / product.reviews.length
-
-  const handleColorSelect = useCallback((value: SelectedImgType) => {}, [cartProduct.selectedImg])
   
+
+  const productRating =
+    product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div>Images</div>
-      <div className="flex flex-col gap-1 text-slate-500 text-sm" >
+      <div className="flex flex-col gap-1 text-slate-500 text-sm">
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
         <div className="flex items-center gap-2">
-          <Rating value={productRating} readOnly/>
+          <Rating value={productRating} readOnly />
           <p>{product.reviews.length} reviews</p>
         </div>
 
@@ -66,12 +77,18 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           <span className="font-semibold">BRAND:</span> {product.brand}
         </div>
 
-        <div className={product.inStock ? 'text-teal-400' : 'text-rose-400'}>{product.inStock ? 'In stock' : 'Out of stock'}</div>
+        <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>
+          {product.inStock ? "In stock" : "Out of stock"}
+        </div>
       </div>
 
       <Horizontal />
 
-      <SetColor images={product.images} cartProduct={cartProduct} handleColorSelect={handleColorSelect}/>
+      <SetColor
+        images={product.images}
+        cartProduct={cartProduct}
+        handleColorSelect={handleColorSelect}
+      />
 
       <Horizontal />
 
@@ -85,3 +102,93 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
 };
 
 export default ProductDetails;
+
+//console.log(cartProduct);
+
+  /*
+  {
+    "id": "648437b38c44d52b9542e340",
+    "name": "Apple iPhone 13, 64GB",
+    "description": "The product is refurbished, fully functional, and in excellent condition.",
+    "category": "Phone",
+    "brand": "Apple",
+    "selectedImg": {
+        "color": "Black",
+        "colorCode": "#000000",
+        "image": "https://m.media-amazon.com/images/I/61g+McQpg7L._AC_SX679_.jpg"
+    },
+    "quantity": 1,
+    "price": 40
+}
+*/
+
+//onsole.log(product);
+
+/*
+{
+  "id": "648437b38c44d52b9542e340",
+  "name": "Apple iPhone 13, 64GB",
+  "description": "The product is refurbished, fully functional, and in excellent condition. ",
+  "price": 40,
+  "brand": "Apple",
+  "category": "Phone",
+  "inStock": true,
+  "images": [
+      {
+          "color": "Black",
+          "colorCode": "#000000",
+          "image": "https://m.media-amazon.com/images/I/61g+McQpg7L._AC_SX679_.jpg"
+      },
+      {
+          "color": "Blue",
+          "colorCode": " #0000FF",
+          "image": "https://m.media-amazon.com/images/I/713Om9vCHUL._AC_SX679_.jpg"
+      },
+      {
+          "color": "Red",
+          "colorCode": "#FF0000",
+          "image": "https://m.media-amazon.com/images/I/61thdjmfHcL.__AC_SX300_SY300_QL70_FMwebp_.jpg"
+      }
+  ],
+  "reviews": [
+      {
+          "id": "6499b4887402b0efd394d8f3",
+          "userId": "6499b184b0e9a8c8709821d3",
+          "productId": "648437b38c44d52b9542e340",
+          "rating": 4,
+          "comment": "good enough. I like the camera and casing. the delivery was fast too.",
+          "createdDate": "2023-06-26T15:53:44.483Z",
+          "user": {
+              "id": "6499b184b0e9a8c8709821d3",
+              "name": "Chaoo",
+              "email": "example1@gmail.com",
+              "emailVerified": null,
+              "image": "https://lh3.googleusercontent.com/a/AAcHTtcuRLwWi1vPKaQOcJlUurlhRAIIq2LgYccE8p32=s96-c",
+              "hashedPassword": null,
+              "createdAt": "2023-06-26T15:40:52.558Z",
+              "updatedAt": "2023-06-26T15:40:52.558Z",
+              "role": "USER"
+          }
+      },
+      {
+          "id": "6499a110efe4e4de451c7edc",
+          "userId": "6475af156bad4917456e6e1e",
+          "productId": "648437b38c44d52b9542e340",
+          "rating": 4,
+          "comment": "I really liked it!!",
+          "createdDate": "2023-06-26T14:30:40.998Z",
+          "user": {
+              "id": "6475af156bad4917456e6e1e",
+              "name": "Charles",
+              "email": "example@gmail.com",
+              "emailVerified": null,
+              "image": "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
+              "hashedPassword": null,
+              "createdAt": "2023-05-30T08:08:53.979Z",
+              "updatedAt": "2023-05-30T08:08:53.979Z",
+              "role": "ADMIN"
+          }
+      }
+  ]
+}
+*/
